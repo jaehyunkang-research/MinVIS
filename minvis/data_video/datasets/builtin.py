f988b77849e5206398dfee26a27f4a300a0ac46d
jaehyunkang-research <abyssinial@naver.com>
2023-09-18 11:22:51 +0000

    data

--- minvis/data_video/datasets/builtin.py
+++ minvis/data_video/datasets/builtin.py
@@ -19,16 +19,24 @@ from .ytvis import (
     _get_ovis_instances_meta,
 )
 
-# ==== Predefined splits for YTVIS 2019 ===========
 _PREDEFINED_SPLITS_YTVIS_2019 = {
-    "ytvis_2019_train": ("ytvis_2019/train/JPEGImages",
-                         "ytvis_2019/train.json"),
-    "ytvis_2019_val": ("ytvis_2019/valid/JPEGImages",
-                       "ytvis_2019/valid.json"),
-    "ytvis_2019_test": ("ytvis_2019/test/JPEGImages",
-                        "ytvis_2019/test.json"),
+    "ytvis_2019_train": ("/workspace/VIS/pseudo_v9_track",
+                       "/workspace/VIS/pseudo_v9_track/annotations_yt.json"),
+    "ytvis_2019_val": ("/workspace/VIS/pseudo_v9_track",
+                       "/workspace/VIS/pseudo_v9_track/annotations_yt.json"),
+    "ytvis_2019_test": ("/workspace/VIS/pseudo_v9_track",
+                       "/workspace/VIS/pseudo_v9_track/annotations_yt.json"),
 }
 
+# ==== Predefined splits for YTVIS 2019 ===========
+# _PREDEFINED_SPLITS_YTVIS_2019 = {
+#     "ytvis_2019_train": ("ytvis_2019/train/JPEGImages",
+#                          "ytvis_2019/train.json"),
+#     "ytvis_2019_val": ("ytvis_2019/valid/JPEGImages",
+#                        "ytvis_2019/valid.json"),
+#     "ytvis_2019_test": ("ytvis_2019/test/JPEGImages",
+#                         "ytvis_2019/test.json"),
+# }
 
 # ==== Predefined splits for YTVIS 2021 ===========
 _PREDEFINED_SPLITS_YTVIS_2021 = {
